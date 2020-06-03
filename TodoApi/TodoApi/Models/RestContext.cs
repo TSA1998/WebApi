1c8f4f4780c202f6e3c219565687dae87bf8f02f
Stepan <52964712+TSA1998@users.noreply.github.com>
2020-06-03 19:12:12 +0300

    Add files via upload

---
+++ TodoApi/TodoApi/Models/RestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestApi.Models
+{
+    public class RestContext : DbContext
+    {
+        public RestContext(DbContextOptions<RestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DishItem> DishItems { get; set; }
+    }
+}
